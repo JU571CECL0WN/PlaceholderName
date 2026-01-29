840bd5fbafc8c2e0e234e7b39e588711eddd26dd
Chaofan Li Huang <li.chaofan.07@gmail.com>
2026-01-29 15:56:07 +0100

    Camera following player now

--- Assets/_Project/Scripts/Player/PlayerMovement.cs
+++ Assets/_Project/Scripts/Player/PlayerMovement.cs
@@ -8,23 +8,32 @@ public class PlayerMovement : NetworkBehaviour
 
     private Rigidbody2D rb;
     private Vector2 moveInput;
-    
-
 
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
+    public override void OnNetworkSpawn()
+    {
+        if (!IsOwner)
+        {
+            rb.simulated = false; 
+            return;
+        }
+        
+        Camera.main.GetComponent<CameraFollow>().target = transform;
+    }
+
     void OnMove(InputValue value)
     {
         if (!IsOwner) return;
-
         moveInput = value.Get<Vector2>();
     }
 
     void FixedUpdate()
     {
+        if (!IsOwner) return; 
         rb.linearVelocity = moveInput * speed;
     }
 }
